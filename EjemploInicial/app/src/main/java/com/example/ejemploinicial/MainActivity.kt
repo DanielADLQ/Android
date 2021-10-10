edcff45ac444c828ed84def532b07ba5034fa494
Daniel ADLQ <danlunaq95@gmail.com>
2021-10-10 11:29:32 +0200

    Commit Inicio

---
+++ EjemploInicial/app/src/main/java/com/example/ejemploinicial/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ejemploinicial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //var miBoton = findViewById(R.id.btnAceptar)
+        //miBoton.setOnClickListener(){
+            //var miCaja:TextView = findViewById(R.id:txtCaja)
+            //miCaja.text = "Cambiado desde codigo"
+            //Toast.makeText(this,"Texto al pulsar,Toast.LENGTH_LONG").show()
+        //}
+    }
+}
